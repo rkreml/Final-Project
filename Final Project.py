004d85f0e8012562bb822f9c5d7f9e5cc1783538
Robert Kreml <kreml006@umn.edu>
2019-12-17 23:20:37 -0600

    Updated to version 1.0.0

--- Final Project.py
+++ Final Project.py
@@ -1,8 +1,8 @@
 #EPSY 5200 Final Project Python Script File
 #Author: Robert T. Kreml
 
-Version = "0.0.1"
-Date = "December 4, 2019"
+Version = "1.0.0"
+Date = "December 17, 2019"
 
 print(f"Thank you for running this script! This is in partial fulfillment of the requirements for EPSY 5200 for the Fall 2019 Semester.")
 print(f"You are currently running version: {Version}, created on: {Date}.")
@@ -10,13 +10,14 @@ print(f"You are currently running version: {Version}, created on: {Date}.")
 import urllib.request
 import urllib.parse
 import json
+import statistics
 
 city = input("Please type in the city you would like to get the weather for: ")
 
 params = {"appid":"7d8615fc2e5e528e24c0a03e480f10e1", "q": city, "units": "imperial"}
 arguments = urllib.parse.urlencode(params)
 
-address = "http://api.openweathermap.org/data/2.5/weather"
+address = "http://api.openweathermap.org/data/2.5/forecast"
 url = address + "?" + arguments
 
 webData = urllib.request.urlopen(url)
@@ -25,11 +26,272 @@ webData.close()
 
 data = json.loads(results)
 
-current = data["main"]
+# print(data["list"][0]["main"]["temp_min"])
+
+## DAY 1 DATA
+
+# creating the first day of the forecast minimum temperatures
+day_1_min = []
+
+day_1_min.append(data["list"][0]["main"]["temp_min"])
+day_1_min.append(data["list"][1]["main"]["temp_min"])
+day_1_min.append(data["list"][2]["main"]["temp_min"])
+day_1_min.append(data["list"][3]["main"]["temp_min"])
+day_1_min.append(data["list"][4]["main"]["temp_min"])
+day_1_min.append(data["list"][5]["main"]["temp_min"])
+day_1_min.append(data["list"][6]["main"]["temp_min"])
+day_1_min.append(data["list"][7]["main"]["temp_min"])
+
+day_1_min_mean = statistics.mean(day_1_min)
+day_1_min_mean = round(day_1_min_mean, ndigits = 2)
+
+# creating the first day of the forecast maximum temperatures
+day_1_max = []
+
+day_1_max.append(data["list"][0]["main"]["temp_max"])
+day_1_max.append(data["list"][1]["main"]["temp_max"])
+day_1_max.append(data["list"][2]["main"]["temp_max"])
+day_1_max.append(data["list"][3]["main"]["temp_max"])
+day_1_max.append(data["list"][4]["main"]["temp_max"])
+day_1_max.append(data["list"][5]["main"]["temp_max"])
+day_1_max.append(data["list"][6]["main"]["temp_max"])
+day_1_max.append(data["list"][7]["main"]["temp_max"])
+
+day_1_max_mean = statistics.mean(day_1_max)
+day_1_max_mean = round(day_1_max_mean, ndigits = 2)
+
+# creating the first day of the forecast humidity
+day_1_hum = []
+
+day_1_hum.append(data["list"][0]["main"]["humidity"])
+day_1_hum.append(data["list"][1]["main"]["humidity"])
+day_1_hum.append(data["list"][2]["main"]["humidity"])
+day_1_hum.append(data["list"][3]["main"]["humidity"])
+day_1_hum.append(data["list"][4]["main"]["humidity"])
+day_1_hum.append(data["list"][5]["main"]["humidity"])
+day_1_hum.append(data["list"][6]["main"]["humidity"])
+day_1_hum.append(data["list"][7]["main"]["humidity"])
+
+day_1_hum_mean = statistics.mean(day_1_hum)
+day_1_hum_mean = round(day_1_hum_mean, ndigits = 2)
+
+## DAY 2 DATA
+
+# creating the second day of the forecast minimum temperatures
+day_2_min = []
+
+day_2_min.append(data["list"][8]["main"]["temp_min"])
+day_2_min.append(data["list"][9]["main"]["temp_min"])
+day_2_min.append(data["list"][10]["main"]["temp_min"])
+day_2_min.append(data["list"][11]["main"]["temp_min"])
+day_2_min.append(data["list"][12]["main"]["temp_min"])
+day_2_min.append(data["list"][13]["main"]["temp_min"])
+day_2_min.append(data["list"][14]["main"]["temp_min"])
+day_2_min.append(data["list"][15]["main"]["temp_min"])
+
+day_2_min_mean = statistics.mean(day_2_min)
+day_2_min_mean = round(day_2_min_mean, ndigits = 2)
+
+# creating the second day of the forecast maximum temperatures
+day_2_max = []
+
+day_2_max.append(data["list"][8]["main"]["temp_max"])
+day_2_max.append(data["list"][9]["main"]["temp_max"])
+day_2_max.append(data["list"][10]["main"]["temp_max"])
+day_2_max.append(data["list"][11]["main"]["temp_max"])
+day_2_max.append(data["list"][12]["main"]["temp_max"])
+day_2_max.append(data["list"][13]["main"]["temp_max"])
+day_2_max.append(data["list"][14]["main"]["temp_max"])
+day_2_max.append(data["list"][15]["main"]["temp_max"])
+
+day_2_max_mean = statistics.mean(day_2_max)
+day_2_max_mean = round(day_2_max_mean, ndigits = 2)
+
+# creating the second day of the forecast humidity
+day_2_hum = []
+
+day_2_hum.append(data["list"][8]["main"]["humidity"])
+day_2_hum.append(data["list"][9]["main"]["humidity"])
+day_2_hum.append(data["list"][10]["main"]["humidity"])
+day_2_hum.append(data["list"][11]["main"]["humidity"])
+day_2_hum.append(data["list"][12]["main"]["humidity"])
+day_2_hum.append(data["list"][13]["main"]["humidity"])
+day_2_hum.append(data["list"][14]["main"]["humidity"])
+day_2_hum.append(data["list"][15]["main"]["humidity"])
+
+day_2_hum_mean = statistics.mean(day_2_hum)
+day_2_hum_mean = round(day_2_hum_mean, ndigits = 2)
+
+## DAY 3 DATA
+
+# creating the third day of the forecast minimum temperatures
+day_3_min = []
+
+day_3_min.append(data["list"][16]["main"]["temp_min"])
+day_3_min.append(data["list"][17]["main"]["temp_min"])
+day_3_min.append(data["list"][18]["main"]["temp_min"])
+day_3_min.append(data["list"][19]["main"]["temp_min"])
+day_3_min.append(data["list"][20]["main"]["temp_min"])
+day_3_min.append(data["list"][21]["main"]["temp_min"])
+day_3_min.append(data["list"][22]["main"]["temp_min"])
+day_3_min.append(data["list"][23]["main"]["temp_min"])
+
+day_3_min_mean = statistics.mean(day_3_min)
+day_3_min_mean = round(day_3_min_mean, ndigits = 2)
+
+# creating the third day of the forecast maximum temperatures
+day_3_max = []
+
+day_3_max.append(data["list"][16]["main"]["temp_max"])
+day_3_max.append(data["list"][17]["main"]["temp_max"])
+day_3_max.append(data["list"][18]["main"]["temp_max"])
+day_3_max.append(data["list"][19]["main"]["temp_max"])
+day_3_max.append(data["list"][20]["main"]["temp_max"])
+day_3_max.append(data["list"][21]["main"]["temp_max"])
+day_3_max.append(data["list"][22]["main"]["temp_max"])
+day_3_max.append(data["list"][23]["main"]["temp_max"])
+
+day_3_max_mean = statistics.mean(day_3_max)
+day_3_max_mean = round(day_3_max_mean, ndigits = 2)
+
+# creating the third day of the forecast humidity
+day_3_hum = []
+
+day_3_hum.append(data["list"][16]["main"]["humidity"])
+day_3_hum.append(data["list"][17]["main"]["humidity"])
+day_3_hum.append(data["list"][18]["main"]["humidity"])
+day_3_hum.append(data["list"][19]["main"]["humidity"])
+day_3_hum.append(data["list"][20]["main"]["humidity"])
+day_3_hum.append(data["list"][21]["main"]["humidity"])
+day_3_hum.append(data["list"][22]["main"]["humidity"])
+day_3_hum.append(data["list"][23]["main"]["humidity"])
+
+day_3_hum_mean = statistics.mean(day_3_hum)
+day_3_hum_mean = round(day_3_hum_mean, ndigits = 2)
+
+## DAY 4 DATA
+
+# creating the fourth day of the forecast minimum temperatures
+day_4_min = []
+
+day_4_min.append(data["list"][24]["main"]["temp_min"])
+day_4_min.append(data["list"][25]["main"]["temp_min"])
+day_4_min.append(data["list"][26]["main"]["temp_min"])
+day_4_min.append(data["list"][27]["main"]["temp_min"])
+day_4_min.append(data["list"][28]["main"]["temp_min"])
+day_4_min.append(data["list"][29]["main"]["temp_min"])
+day_4_min.append(data["list"][30]["main"]["temp_min"])
+day_4_min.append(data["list"][31]["main"]["temp_min"])
+
+day_4_min_mean = statistics.mean(day_4_min)
+day_4_min_mean = round(day_4_min_mean, ndigits = 2)
+
+# creating the fourth day of the forecast maximum temperatures
+day_4_max = []
+
+day_4_max.append(data["list"][24]["main"]["temp_max"])
+day_4_max.append(data["list"][25]["main"]["temp_max"])
+day_4_max.append(data["list"][26]["main"]["temp_max"])
+day_4_max.append(data["list"][27]["main"]["temp_max"])
+day_4_max.append(data["list"][28]["main"]["temp_max"])
+day_4_max.append(data["list"][29]["main"]["temp_max"])
+day_4_max.append(data["list"][30]["main"]["temp_max"])
+day_4_max.append(data["list"][31]["main"]["temp_max"])
+
+day_4_max_mean = statistics.mean(day_4_max)
+day_4_max_mean = round(day_4_max_mean, ndigits = 2)
+
+# creating the fourth day of the forecast humidity
+day_4_hum = []
+
+day_4_hum.append(data["list"][24]["main"]["humidity"])
+day_4_hum.append(data["list"][25]["main"]["humidity"])
+day_4_hum.append(data["list"][26]["main"]["humidity"])
+day_4_hum.append(data["list"][27]["main"]["humidity"])
+day_4_hum.append(data["list"][28]["main"]["humidity"])
+day_4_hum.append(data["list"][29]["main"]["humidity"])
+day_4_hum.append(data["list"][30]["main"]["humidity"])
+day_4_hum.append(data["list"][31]["main"]["humidity"])
+
+day_4_hum_mean = statistics.mean(day_4_hum)
+day_4_hum_mean = round(day_4_hum_mean, ndigits = 2)
+
+## DAY 5 DATA
+
+# creating the fifth day of the forecast minimum temperatures
+day_5_min = []
+
+day_5_min.append(data["list"][32]["main"]["temp_min"])
+day_5_min.append(data["list"][33]["main"]["temp_min"])
+day_5_min.append(data["list"][34]["main"]["temp_min"])
+day_5_min.append(data["list"][35]["main"]["temp_min"])
+day_5_min.append(data["list"][36]["main"]["temp_min"])
+day_5_min.append(data["list"][37]["main"]["temp_min"])
+day_5_min.append(data["list"][38]["main"]["temp_min"])
+day_5_min.append(data["list"][39]["main"]["temp_min"])
+
+day_5_min_mean = statistics.mean(day_5_min)
+day_5_min_mean = round(day_5_min_mean, ndigits = 2)
+
+# creating the fifth day of the forecast maximum temperatures
+day_5_max = []
+
+day_5_max.append(data["list"][32]["main"]["temp_max"])
+day_5_max.append(data["list"][33]["main"]["temp_max"])
+day_5_max.append(data["list"][34]["main"]["temp_max"])
+day_5_max.append(data["list"][35]["main"]["temp_max"])
+day_5_max.append(data["list"][36]["main"]["temp_max"])
+day_5_max.append(data["list"][37]["main"]["temp_max"])
+day_5_max.append(data["list"][38]["main"]["temp_max"])
+day_5_max.append(data["list"][39]["main"]["temp_max"])
+
+day_5_max_mean = statistics.mean(day_5_max)
+day_5_max_mean = round(day_5_max_mean, ndigits = 2)
+
+# creating the fifth day of the forecast humidity
+day_5_hum = []
+
+day_5_hum.append(data["list"][32]["main"]["humidity"])
+day_5_hum.append(data["list"][33]["main"]["humidity"])
+day_5_hum.append(data["list"][34]["main"]["humidity"])
+day_5_hum.append(data["list"][35]["main"]["humidity"])
+day_5_hum.append(data["list"][36]["main"]["humidity"])
+day_5_hum.append(data["list"][37]["main"]["humidity"])
+day_5_hum.append(data["list"][38]["main"]["humidity"])
+day_5_hum.append(data["list"][39]["main"]["humidity"])
+
+day_5_hum_mean = statistics.mean(day_5_hum)
+day_5_hum_mean = round(day_5_hum_mean, ndigits = 2)
+
+## Degree Symbol
 degreeSym = chr(176)
 
-print(f"The current weather in {city} is the following:")
-print(f'The current temperature: {current["temp"]}{degreeSym}F')
-print(f"The minimum temperature today in {city} is: {current['temp_min']}{degreeSym}F")
-print(f"The high temperature today in {city} is: {current['temp_max']}{degreeSym}F")
-print(f'{city} is currently experiencing {current["humidity"]}% humidity')
+# Printing the results in a readable format for the user.
+print("\n")
+print(f"The next five day forecast in {city} is:")
+print("\n")
+print("Tomorrow's weather is:")
+print(f"The average minimum temperature will be: {day_1_min_mean}{degreeSym}F.")
+print(f"The average maximum temperature will be: {day_1_max_mean}{degreeSym}F.")
+print(f"The average humidity will be: {day_1_hum_mean}.")
+print("\n")
+print("Day 2's weather will be:")
+print(f"The average minimum temperature will be: {day_2_min_mean}{degreeSym}F.")
+print(f"The average maximum temperature will be: {day_2_max_mean}{degreeSym}F.")
+print(f"The average humidity will be: {day_2_hum_mean}.")
+print("\n")
+print("Day 3's weather will be:")
+print(f"The average minimum temperature will be: {day_3_min_mean}{degreeSym}F.")
+print(f"The average maximum temperature will be: {day_3_max_mean}{degreeSym}F.")
+print(f"The average humidity will be: {day_3_hum_mean}.")
+print("\n")
+print("Day 4's weather will be:")
+print(f"The average minimum temperature will be: {day_4_min_mean}{degreeSym}F.")
+print(f"The average maximum temperature will be: {day_4_max_mean}{degreeSym}F.")
+print(f"The average humidity will be: {day_4_hum_mean}.")
+print("\n")
+print("Day 5's weather will be:")
+print(f"The average minimum temperature will be: {day_5_min_mean}{degreeSym}F.")
+print(f"The average maximum temperature will be: {day_5_max_mean}{degreeSym}F.")
+print(f"The average humidity will be: {day_5_hum_mean}.")
+print("\n")
--- README.md
+++ README.md
@@ -18,3 +18,24 @@ Bugs still Present:
 Lessons Learned:
     - Getting wiki to work will be a bit more challenging. Also implementing user input is tricky. I will want to include some error messages just in case.
 
+Version 0.0.2
+Features:
+    - Included version control and hooked into github.
+    - Changed the weather script from assignment 6 to include a 5 day forcast.
+    - Added the mins and maxes for each of the 5 day forcast.
+    - Retained current temperature for that particular day.
+    - Retained current humidity percentage for current day.
+Bugs still Present:
+    - Still need to work on slicing the data of dictionary within a dictionary. So no data will currently work.
+Lessons Learned:
+    - Be careful with url typing to ensure the format of the URL is being followed, otherwise the dreaded 404 error will appear. Parameters may need to change depending on the type of API repository you are pulling information from.
+    - Dictionaries can get extremely complicated when subset dictionaries are the same name as each other and the "key" is a unique key identifier.
+
+Version 1.0.0
+Features:
+    - Includes a day-by-day breakdown of the minimum and maximum average temperature for all 5 days in the forecast.
+    - Keep in mind that the minimum and maximum temperature for days 2 - 5 are the same as the weather API cannot accurately predict those temperature ranges.
+Bugs still present:
+    - (Not a bug): I still want to add additional locations/cities through user input.
+Lessons learned:
+    - Indexing and slicing large dictionaries is difficult.
